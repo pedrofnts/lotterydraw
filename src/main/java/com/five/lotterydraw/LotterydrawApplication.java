abec3430341fda7ce9d8b914121654aaaf85ca0f
Pedro Fontes <pedro7fontes@gmail.com>
2023-03-01 21:46:07 -0300

    initial commit

---
+++ src/main/java/com/five/lotterydraw/LotterydrawApplication.java
@@ -0,0 +1,13 @@
+package com.five.lotterydraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterydrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterydrawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/five/lotterydraw/LotterydrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.five.lotterydraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotterydrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
