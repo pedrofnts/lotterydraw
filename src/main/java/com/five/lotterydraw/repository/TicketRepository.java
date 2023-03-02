5bc1a6dee888d623fab8187c39fc0de14eab9ad0
Pedro Fontes <pedro7fontes@gmail.com>
2023-03-02 16:55:46 -0300

    feat: add method to list tickets by user id

--- src/main/java/com/five/lotterydraw/repository/TicketRepository.java
+++ src/main/java/com/five/lotterydraw/repository/TicketRepository.java
@@ -3,6 +3,8 @@ package com.five.lotterydraw.repository;
 import com.five.lotterydraw.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+import java.util.List;
 
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findAllByOwner_Id(Long id);
 }
